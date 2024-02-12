bae9418c542d9c71819ec05ca583cc8d6e785e04
Lover Atomical <khahare0@gmail.com>
2024-02-12 19:00:03 +0545

    added while loop indexing

--- src/Indexer/Process-Indexer.ts
+++ src/Indexer/Process-Indexer.ts
@@ -8,9 +8,13 @@ import InscriptionsWorker from "./Inscription-Worker";
 import Logger from "../Shared/indexer-helper/logger";
 
 let LastBlock: number = 0;
-
+let LatestBlock: number = 0;
 const StartIndexer = async () => {
   try {
+    const LatestBlock_ = await IndexerQuery.GetLatestBlock();
+
+    LatestBlock = LatestBlock_;
+
     if (LastBlock === 0) {
       Logger.Success(`Starting Indexer Core to Process Indexing Work.....`);
 
@@ -28,80 +32,72 @@ const StartIndexer = async () => {
         );
         LastBlock = LastIndexedBlock;
       }
-    } else {
+    }
+
+    while (true) {
+      //Checking if we are sarting to index from back 0 or last saved block
       Logger.Success(`Starting to Index form Block ${LastBlock}`);
-    }
 
-    const StartingBlock: number = LastBlock;
+      const StartingBlock: number = LastBlock;
 
-    const LatestBlock = await IndexerQuery.GetLatestBlock();
+      if (new Decimal(LatestBlock).lte(StartingBlock) || LatestBlock === 0) {
+        Logger.Success(`Trying to Sleep for 30sec before indexing new Block`);
+        LatestBlock = await IndexerQuery.GetLatestBlock();
+        await Sleep(30);
+        continue;
+      }
 
-    if (LatestBlock === undefined)
-      throw new Error("Faild to Load Saved Block!");
+      const BlocksToIndex: number[] = [];
 
-    if (new Decimal(LatestBlock).lte(StartingBlock)) {
-      Logger.Success(`Trying to Sleep for 30sec before indexing new Block`);
+      for (let i = 0; i < MaxBlock; i++) {
+        BlocksToIndex.push(StartingBlock + i);
+      }
 
-      await Sleep(30);
-      await StartIndexer();
-    }
+      const BlockData = await DataQuery.LoadInscriptions(BlocksToIndex);
 
-    const BlocksToIndex: number[] = [];
+      LastBlock = BlocksToIndex[BlocksToIndex.length - 1] + 1;
 
-    for (let i = 0; i < MaxBlock; i++) {
-      BlocksToIndex.push(StartingBlock + i);
-    }
-
-    const BlockData = await DataQuery.LoadInscriptions(BlocksToIndex);
-
-    LastBlock = BlocksToIndex[BlocksToIndex.length - 1] + 1;
-
-    Logger.Success(
-      `Scanned Blocks from ${BlocksToIndex[0]} to ${
-        BlocksToIndex[BlocksToIndex.length - 1]
-      }`
-    );
-
-    if (!BlockData) {
-      Logger.error(
-        `Not Single Inscription found between blocks ${BlocksToIndex[0]} to ${
+      Logger.Success(
+        `Scanned Blocks from ${BlocksToIndex[0]} to ${
           BlocksToIndex[BlocksToIndex.length - 1]
-        }  `
+        }`
       );
-      await Sleep(1); //sleep for 5sec and reindex
 
-      await StartIndexer();
-      return;
-    }
+      if (!BlockData) {
+        Logger.error(
+          `Not Single Inscription found between blocks ${BlocksToIndex[0]} to ${
+            BlocksToIndex[BlocksToIndex.length - 1]
+          }  `
+        );
 
-    const FormatedInscriptionData = await InscriptionsWorker(
-      BlockData,
-      BlocksToIndex
-    );
+        continue;
+      }
 
-    Logger.Success(`Pharsing Blocks Inscription data before Indexing....`);
-
-    if (!FormatedInscriptionData.length) {
-      Logger.error(
-        `No Valid DRC-20 Inscription found between blocks ${
-          BlocksToIndex[0]
-        } to ${BlocksToIndex[BlocksToIndex.length - 1]}  `
+      const FormatedInscriptionData = await InscriptionsWorker(
+        BlockData,
+        BlocksToIndex
       );
-      await Sleep(1); //sleep for 5sec and reindex
 
-      await StartIndexer();
+      Logger.Success(`Pharsing Blocks Inscription data before Indexing....`);
+
+      if (!FormatedInscriptionData.length) {
+        Logger.error(
+          `No Valid DRC-20 Inscription found between blocks ${
+            BlocksToIndex[0]
+          } to ${BlocksToIndex[BlocksToIndex.length - 1]}  `
+        );
+
+        continue;
+      }
+
+      Logger.Success(`Stating Inscription Indexing Worker.....`);
+
+      await IndexDoginals(FormatedInscriptionData);
+
+      Logger.Success(`Successfully Indexed the Inscription from the Blocks `);
+
+      await IndexerQuery.UpdatedLastScannedBlock(LastBlock);
     }
-
-    Logger.Success(`Stating Inscription Indexing Worker.....`);
-
-    await IndexDoginals(FormatedInscriptionData);
-
-    Logger.Success(`Successfully Indexed the Inscription from the Blocks `);
-
-    await IndexerQuery.UpdatedLastScannedBlock(LastBlock);
-
-    await Sleep(1); //sleep for 5sec and reindex
-    await StartIndexer();
   } catch (error) {
     throw error;
   }
